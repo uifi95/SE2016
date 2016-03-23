3bad285d23f0904ef1cf60fb4272bdaca04d2c23
Uifalean Sergiu <usergiu95@gmail.com>
2016-03-24 00:07:39 +0200

    First commit,fixed pycharm annoying errors,it seems we can use only the django template folder,but we can have subfolders there,waiting for opinions

--- academic/LoginApp/urls.py
+++ academic/LoginApp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name= "LoginApp"
+app_name = "LoginApp"
 urlpatterns = [
     url(r'^$', views.main_page, name='main'),
     url(r'^login/', views.login_page, name='login'),
--- academic/LoginApp/views.py
+++ academic/LoginApp/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib import admin
 
 # Create your views here.
-from LoginApp.forms import LoginForm, CustomizeAccountForm
 from django.forms import ValidationError
+
+from LoginApp.forms import LoginForm, CustomizeAccountForm
+
 
 @login_required(login_url=reverse_lazy('LoginApp:login'))
 def main_page(request):
@@ -23,12 +24,12 @@ def login_page(request):
         return redirect('LoginApp:main')
     if request.method != 'POST':
         newForm = LoginForm()
-        return render(request, "LoginApp/login.html", {'form':newForm})
+        return render(request, "LoginApp/login.html", {'form': newForm})
     authForm = LoginForm(data=request.POST)
     if authForm.is_valid():
         username = authForm.cleaned_data["username"]
         password = authForm.cleaned_data["password"]
-        user = authenticate(username=username, password = password)
+        user = authenticate(username=username, password=password)
         if not user:
             authForm.add_error(None, ValidationError("User or password wrong!"))
             return render(request, "LoginApp/login.html", {'form': authForm})
@@ -39,13 +40,13 @@ def login_page(request):
         else:
             return HttpResponseRedirect(request.GET["next"])
     else:
-         return render(request, "LoginApp/login.html", {'form': authForm})
+        return render(request, "LoginApp/login.html", {'form': authForm})
 
 
 @login_required(login_url=reverse_lazy('LoginApp:login'))
 def logout_page(request):
     logout(request)
-    return render(request, "LoginApp/logout.html", {"title" : "Logged out!"})
+    return render(request, "LoginApp/logout.html", {"title": "Logged out!"})
 
 
 @login_required(login_url=reverse_lazy('LoginApp:login'))
@@ -53,7 +54,7 @@ def change_account(request):
     if request.method != 'POST':
         newForm = CustomizeAccountForm(user=request.user)
         newForm.fields["username"].initial = request.user.username;
-        return render(request, "LoginApp/change_account.html", {'form':newForm})
+        return render(request, "LoginApp/change_account.html", {'form': newForm})
 
     form = CustomizeAccountForm(user=request.user, data=request.POST)
     if form.is_valid():
@@ -66,4 +67,4 @@ def change_account(request):
             obj.save()
         return render(request, "LoginApp/done_change_account.html", {'title': "Account updated!"})
     else:
-        return render(request, "LoginApp/change_account.html", {'form':form})
+        return render(request, "LoginApp/change_account.html", {'form': form})
