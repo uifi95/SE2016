1f137229ab49c1652a80121290235566f662de3e
Vlad Lazar <vlad.lazar1994@gmail.com>
2016-03-23 20:26:27 +0200

    Restricted access to students based on user type

--- academic/StudentApp/views.py
+++ academic/StudentApp/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 
 # Create your views here.
+from LoginApp.user_checks import student_check
 
 
 @login_required(login_url=reverse_lazy('LoginApp:login'))
+@user_passes_test(student_check, login_url=reverse_lazy('LoginApp:login'))
 def main_page(request):
     return render(request, "StudentApp/student_main.html", {"has_permission" : True})
