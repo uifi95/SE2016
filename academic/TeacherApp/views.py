e16390b60a682b92998408fe878e401c974c8cf8
Uifalean Sergiu <usergiu95@gmail.com>
2016-03-29 06:13:21 +0300

    Fix#5,have to change gitignore

--- academic/TeacherApp/views.py
+++ academic/TeacherApp/views.py
@@ -19,19 +19,20 @@ def teacher_main(request):
     name = Teacher.objects.all().filter(user=request.user).first().last_name
     return render(request, 'TeacherApp/teacher_main.html', {"first_name": name, "has_permission": True})
 
+
 @login_required(login_url=reverse_lazy('LoginApp:login'))
 @user_passes_test(teacher_check, login_url=reverse_lazy('LoginApp:login'))
 def grades(request):
-    if(request.GET.get('sel')):
-        value = request.GET.get('grade')
-        studentname = request.GET.get('selectedstudent')
-        stlastname= studentname.split(" ")[0]
-        student = Student.objects.all().filter(first_name=stlastname)
-        coursename = request.GET.get('selectedcourse')
-        course = Course.objects.all().filter(name=coursename)
-        grade = Grade(value=value,student=student[0],course=course[0])
+    if request.GET.get('select_button'):
+        value = int(request.GET.get('grade'))
+        student_name = request.GET.get('selected_student')
+        first_name = student_name.split(" ")[0]
+        student = Student.objects.all().filter(first_name=first_name)
+        course_name = request.GET.get('selected_course')
+        course = Course.objects.all().filter(name=course_name)
+        grade = Grade(value=value, student=student[0], course=course[0])
         grade.save()
     current_teacher = request.user.client_set.first()
-    students = Student.objects.all()
     courses = Course.objects.all().filter(teacher=current_teacher)
-    return render(request, "TeacherApp/grades.html", {"courses": courses,"students":students, "has_permission": True})
+    students = Student.objects.all()
+    return render(request, "TeacherApp/grades.html", {"courses": courses, "students": students, "has_permission": True})
