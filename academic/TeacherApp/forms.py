cf29ba58b98c6a5df2c1828438acd9f3d4f73184
LisaFort <moismelisa@yahoo.com>
2016-04-06 15:54:31 +0300

    Fixed migrations bug

--- academic/TeacherApp/forms.py
+++ academic/TeacherApp/forms.py
@@ -2,6 +2,3 @@ from django import forms
 from TeacherApp.models import Course
 from django.forms import Form
 
-
-class selectcoursesform(Form):
-        courses = forms.ChoiceField(Course.objects.all())
