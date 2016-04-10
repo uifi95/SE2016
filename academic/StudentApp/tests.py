b5eaecac2428d1e18e3c313887b311bf505191f9
Uifalean Sergiu <usergiu95@gmail.com>
2016-04-11 01:15:12 +0300

    Added tests for the views in Teacher and StudentApp

--- academic/StudentApp/tests.py
+++ academic/StudentApp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class ViewTests(TestCase):
+    def test_access(self):
+        c = self.client
+        response = c.get("/student/grades/")
+        self.assertEquals(response.status_code, 302)
--- academic/TeacherApp/tests.py
+++ academic/TeacherApp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class ViewTests(TestCase):
+    def test_access(self):
+        c = self.client
+        response = c.get("/teacher/courses/")
+        self.assertEquals(response.status_code, 302)
+        response = c.get("/teacher/optionals/")
+        self.assertEquals(response.status_code, 302)
+        response = c.get("/teacher/courses/")
+        self.assertEquals(response.status_code, 302)
+        response = c.get("/teacher/optionals/add/")
+        self.assertEquals(response.status_code, 302)
+        response = c.get("/teacher/optionals/delete/")
+        self.assertEquals(response.status_code, 404)
